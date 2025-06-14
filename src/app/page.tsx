160eb47457a7cf901e8077e00972650384746ad1
artorias443 <artorias443@outlook.com>
2025-06-14 16:55:50 +0800

    3-2 安装 Button 组件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return <div>Root Page</div>
+  return (
+    <div>
+      Root Page
+      <Button variant="ghost" type="submit">
+        Click me
+      </Button>
+    </div>
+  )
 }
